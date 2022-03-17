517cfda365036eb914c6ca345eefccc4ce5ca154
johnlawliet <t.shiladitya@gmail.com>
2022-03-18 00:48:58 +0530

    added graphs

--- basic.py
+++ basic.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas
+
+data = {
+    'Series_1':[1,3,4,5,7],
+    'Series_2':[10,20,30,40,100,250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Our first streamlit app')
 st.subheader('introducing streamlit in automate')
 st.write('''Bruh This is streamlit. ENJOY ITTT!!''')
+
+st.write(df)
+st.line_chart(df)
